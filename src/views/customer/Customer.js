c90e53cdfdbde167f9c3e1c9e357c4f4b989ffa3
Recep Dur <recep.dur@kuveytturk.com.tr>
2022-07-25 19:38:25 +0300

    customer api entegration

--- src/views/customer/Customer.js
+++ src/views/customer/Customer.js
@@ -6,51 +6,34 @@ import './index.css'
 import ReactDOM from 'react-dom'
 
 import React, { useState, useEffect } from 'react'
-import { classNames } from 'primereact/utils'
 import { FilterMatchMode, FilterOperator } from 'primereact/api'
 import { DataTable } from 'primereact/datatable'
 import { Column } from 'primereact/column'
 import { InputText } from 'primereact/inputtext'
-import { Dropdown } from 'primereact/dropdown'
 import { InputNumber } from 'primereact/inputnumber'
 import { Button } from 'primereact/button'
-import { ProgressBar } from 'primereact/progressbar'
 import { Calendar } from 'primereact/calendar'
-import { MultiSelect } from 'primereact/multiselect'
-import { Slider } from 'primereact/slider'
-import { TriStateCheckbox } from 'primereact/tristatecheckbox'
 import { CustomerService } from './CustomerService'
 import './DataTableDemo.css'
+import { execute } from '../../common/basePage'
+import { Menubar } from 'primereact/menubar'
 
 const Customer = () => {
-  const [customers1, setCustomers1] = useState(null)
-  const [filters1, setFilters1] = useState(null)
-
-  const [globalFilterValue1, setGlobalFilterValue1] = useState('')
-  const [loading1, setLoading1] = useState(true)
-  const representatives = [
-    { name: 'Amy Elsner', image: 'amyelsner.png' },
-    { name: 'Anna Fali', image: 'annafali.png' },
-    { name: 'Asiya Javayant', image: 'asiyajavayant.png' },
-    { name: 'Bernardo Dominic', image: 'bernardodominic.png' },
-    { name: 'Elwin Sharvill', image: 'elwinsharvill.png' },
-    { name: 'Ioni Bowcher', image: 'ionibowcher.png' },
-    { name: 'Ivan Magalhaes', image: 'ivanmagalhaes.png' },
-    { name: 'Onyama Limba', image: 'onyamalimba.png' },
-    { name: 'Stephen Shaw', image: 'stephenshaw.png' },
-    { name: 'XuXue Feng', image: 'xuxuefeng.png' },
-  ]
-
-  const statuses = ['unqualified', 'qualified', 'new', 'negotiation', 'renewal', 'proposal']
+  const [customers, setCustomers] = useState(null)
+  const [filters, setFilters] = useState(null)
+  const [globalFilterValue, setGlobalFilterValue] = useState('')
+  const [loading, setLoading] = useState(true)
+  const [selectedProduct5, setSelectedProduct5] = useState(null)
 
   const customerService = new CustomerService()
 
   useEffect(() => {
-    customerService.getCustomersLarge().then((data) => {
-      setCustomers1(getCustomers(data))
-      setLoading1(false)
-    })
-    initFilters1()
+    // customerService.getCustomersLarge().then((data) => {
+    //   setCustomers(getCustomers(data))
+    //   setLoading(false)
+    // })
+    getCustomerList(true)
+    initFilters()
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
   const getCustomers = (data) => {
@@ -60,177 +43,110 @@ const Customer = () => {
     })
   }
 
-  const formatDate = (value) => {
-    return value.toLocaleDateString('en-US', {
-      day: '2-digit',
-      month: '2-digit',
-      year: 'numeric',
+  const getCustomerList = async (isAct) => {
+    setLoading(true)
+    const cusData = {
+      methodName: 'SelectByColumns',
+      data: {
+        userId: localStorage.getItem('loginUserId'),
+        isActive: isAct,
+      },
+    }
+    execute('/api/customers', 'POST', cusData, false, (response) => {
+      if (response.success) {
+        setCustomers(response.data)
+      } else {
+        // callAlert(response.message, 3, 'danger')
+      }
+      setLoading(false)
     })
+  }
+
+  const formatDate = (value) => {
+    return value
+    // return value.toLocaleDateString('en-US', {
+    //   day: '2-digit',
+    //   month: '2-digit',
+    //   year: 'numeric',
+    // })
   }
 
   const formatCurrency = (value) => {
     return value.toLocaleString('en-US', { style: 'currency', currency: 'USD' })
   }
 
-  const clearFilter1 = () => {
-    initFilters1()
+  const clearFilter = () => {
+    initFilters()
   }
 
-  const onGlobalFilterChange1 = (e) => {
+  const onGlobalFilterChange = (e) => {
     const value = e.target.value
-    let _filters1 = { ...filters1 }
-    _filters1['global'].value = value
-
-    setFilters1(_filters1)
-    setGlobalFilterValue1(value)
+    let _filters = { ...filters }
+    _filters['global'].value = value
+    setFilters(_filters)
+    setGlobalFilterValue(value)
   }
 
-  const initFilters1 = () => {
-    setFilters1({
+  const initFilters = () => {
+    setFilters({
       global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-      name: {
+      first_name: {
         operator: FilterOperator.AND,
         constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }],
       },
-      'country.name': {
+      last_name: {
         operator: FilterOperator.AND,
         constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }],
       },
-      representative: { value: null, matchMode: FilterMatchMode.IN },
-      date: {
+      phone: {
         operator: FilterOperator.AND,
-        constraints: [{ value: null, matchMode: FilterMatchMode.DATE_IS }],
+        constraints: [{ value: null, matchMode: FilterMatchMode.CONTAINS }],
       },
-      balance: {
+      tc_no: {
         operator: FilterOperator.AND,
-        constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }],
+        constraints: [{ value: null, matchMode: FilterMatchMode.CONTAINS }],
       },
-      status: {
-        operator: FilterOperator.OR,
-        constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }],
-      },
-      activity: { value: null, matchMode: FilterMatchMode.BETWEEN },
-      verified: { value: null, matchMode: FilterMatchMode.EQUALS },
+      // name: {
+      //   operator: FilterOperator.AND,
+      //   constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }],
+      // },
+      // date: {
+      //   operator: FilterOperator.AND,
+      //   constraints: [{ value: null, matchMode: FilterMatchMode.DATE_IS }],
+      // },
+      // balance: {
+      //   operator: FilterOperator.AND,
+      //   constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }],
+      // },
     })
-    setGlobalFilterValue1('')
+    setGlobalFilterValue('')
   }
 
-  const renderHeader1 = () => {
+  const renderHeader = () => {
     return (
-      <div className="flex justify-content-between">
-        <Button
-          type="button"
-          icon="pi pi-filter-slash"
-          label="Clear"
-          className="p-button-outlined"
-          onClick={clearFilter1}
-        />
-        <span className="p-input-icon-left">
+      <div className="flex justify-content-center">
+        <span className="p-input-icon-left mr-2 w-50">
           <i className="pi pi-search" />
           <InputText
-            value={globalFilterValue1}
-            onChange={onGlobalFilterChange1}
-            placeholder="Keyword Search"
+            value={globalFilterValue}
+            onChange={onGlobalFilterChange}
+            placeholder="Arama"
+            className="p-inputtext-sm block mb-2 w-75"
           />
         </span>
-      </div>
-    )
-  }
-
-  const countryBodyTemplate = (rowData) => {
-    return (
-      <React.Fragment>
-        <img
-          alt="flag"
-          src="/images/flag/flag_placeholder.png"
-          onError={(e) =>
-            (e.target.src = 'https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png')
-          }
-          className={`flag flag-${rowData.country.code}`}
-          width={30}
-        />
-        <span className="image-text">{rowData.country.name}</span>
-      </React.Fragment>
-    )
-  }
-
-  const filterClearTemplate = (options) => {
-    return (
-      <Button
-        type="button"
-        icon="pi pi-times"
-        onClick={options.filterClearCallback}
-        className="p-button-secondary"
-      ></Button>
-    )
-  }
-
-  const filterApplyTemplate = (options) => {
-    return (
-      <Button
-        type="button"
-        icon="pi pi-check"
-        onClick={options.filterApplyCallback}
-        className="p-button-success"
-      ></Button>
-    )
-  }
-
-  const filterFooterTemplate = () => {
-    return <div className="px-3 pt-0 pb-3 text-center font-bold">Customized Buttons</div>
-  }
-
-  const representativeBodyTemplate = (rowData) => {
-    const representative = rowData.representative
-    return (
-      <React.Fragment>
-        <img
-          alt={representative.name}
-          src={`images/avatar/${representative.image}`}
-          onError={(e) =>
-            (e.target.src = 'https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png')
-          }
-          width={32}
-          style={{ verticalAlign: 'middle' }}
-        />
-        <span className="image-text">{representative.name}</span>
-      </React.Fragment>
-    )
-  }
-
-  const representativeFilterTemplate = (options) => {
-    return (
-      <MultiSelect
-        value={options.value}
-        options={representatives}
-        itemTemplate={representativesItemTemplate}
-        onChange={(e) => options.filterCallback(e.value)}
-        optionLabel="name"
-        placeholder="Any"
-        className="p-column-filter"
-      />
-    )
-  }
-
-  const representativesItemTemplate = (option) => {
-    return (
-      <div className="p-multiselect-representative-option">
-        <img
-          alt={option.name}
-          src={`images/avatar/${option.image}`}
-          onError={(e) =>
-            (e.target.src = 'https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png')
-          }
-          width={32}
-          style={{ verticalAlign: 'middle' }}
-        />
-        <span className="image-text">{option.name}</span>
+        {/* <Button
+          type="button"
+          icon="pi pi-filter-slash"
+          label="Temizle"
+          className="p-button-outlined p-button-sm"
+          onClick={clearFilter}
+        /> */}
       </div>
     )
   }
 
   const dateBodyTemplate = (rowData) => {
-    return formatDate(rowData.date)
+    return formatDate(rowData.created_at)
   }
 
   const dateFilterTemplate = (options) => {
@@ -261,165 +177,149 @@ const Customer = () => {
     )
   }
 
-  const statusBodyTemplate = (rowData) => {
-    return <span className={`customer-badge status-${rowData.status}`}>{rowData.status}</span>
-  }
+  const menuItems = [
+    {
+      label: 'Müşteri',
+      icon: 'pi pi-fw pi-file',
+      items: [
+        {
+          label: 'Yeni',
+          icon: 'pi pi-fw pi-plus',
+        },
+        {
+          label: 'Güncelle',
+          icon: 'pi pi-fw pi-pencil',
+        },
+        {
+          separator: true,
+        },
+        {
+          label: 'Sil',
+          icon: 'pi pi-fw pi-trash',
+        },
+      ],
+    },
+    {
+      label: 'Sigorta',
+      icon: 'pi pi-fw pi-pencil',
+      items: [
+        {
+          label: 'Left',
+          icon: 'pi pi-fw pi-align-left',
+        },
+        {
+          label: 'Right',
+          icon: 'pi pi-fw pi-align-right',
+        },
+        {
+          label: 'Center',
+          icon: 'pi pi-fw pi-align-center',
+        },
+        {
+          label: 'Justify',
+          icon: 'pi pi-fw pi-align-justify',
+        },
+      ],
+    },
+    {
+      label: 'Hesap',
+      icon: 'pi pi-fw pi-calendar',
+      items: [
+        {
+          label: 'New',
+          icon: 'pi pi-fw pi-user-plus',
+        },
+        {
+          label: 'Delete',
+          icon: 'pi pi-fw pi-user-minus',
+        },
+        {
+          label: 'Search',
+          icon: 'pi pi-fw pi-users',
+          items: [
+            {
+              label: 'Filter',
+              icon: 'pi pi-fw pi-filter',
+              items: [
+                {
+                  label: 'Print',
+                  icon: 'pi pi-fw pi-print',
+                },
+              ],
+            },
+            {
+              icon: 'pi pi-fw pi-bars',
+              label: 'List',
+            },
+          ],
+        },
+      ],
+    },
+  ]
 
-  const statusFilterTemplate = (options) => {
-    return (
-      <Dropdown
-        value={options.value}
-        options={statuses}
-        onChange={(e) => options.filterCallback(e.value, options.index)}
-        itemTemplate={statusItemTemplate}
-        placeholder="Select a Status"
-        className="p-column-filter"
-        showClear
-      />
-    )
-  }
-
-  const statusItemTemplate = (option) => {
-    return <span className={`customer-badge status-${option}`}>{option}</span>
-  }
-
-  const activityBodyTemplate = (rowData) => {
-    return <ProgressBar value={rowData.activity} showValue={false}></ProgressBar>
-  }
-
-  const activityFilterTemplate = (options) => {
-    return (
-      <React.Fragment>
-        <Slider
-          value={options.value}
-          onChange={(e) => options.filterCallback(e.value)}
-          range
-          className="m-3"
-        ></Slider>
-        <div className="flex align-items-center justify-content-between px-2">
-          <span>{options.value ? options.value[0] : 0}</span>
-          <span>{options.value ? options.value[1] : 100}</span>
-        </div>
-      </React.Fragment>
-    )
-  }
-
-  const verifiedBodyTemplate = (rowData) => {
-    return (
-      <i
-        className={classNames('pi', {
-          'true-icon pi-check-circle': rowData.verified,
-          'false-icon pi-times-circle': !rowData.verified,
-        })}
-      ></i>
-    )
-  }
-
-  const verifiedFilterTemplate = (options) => {
-    return (
-      <TriStateCheckbox value={options.value} onChange={(e) => options.filterCallback(e.value)} />
-    )
-  }
-
-  const representativeRowFilterTemplate = (options) => {
-    return (
-      <MultiSelect
-        value={options.value}
-        options={representatives}
-        itemTemplate={representativesItemTemplate}
-        onChange={(e) => options.filterApplyCallback(e.value)}
-        optionLabel="name"
-        placeholder="Any"
-        className="p-column-filter"
-        maxSelectedLabels={1}
-      />
-    )
-  }
-
-  const statusRowFilterTemplate = (options) => {
-    return (
-      <Dropdown
-        value={options.value}
-        options={statuses}
-        onChange={(e) => options.filterApplyCallback(e.value)}
-        itemTemplate={statusItemTemplate}
-        placeholder="Select a Status"
-        className="p-column-filter"
-        showClear
-      />
-    )
-  }
-
-  const verifiedRowFilterTemplate = (options) => {
-    return (
-      <TriStateCheckbox
-        value={options.value}
-        onChange={(e) => options.filterApplyCallback(e.value)}
-      />
-    )
-  }
-
-  const header1 = renderHeader1()
+  const header = renderHeader()
 
   return (
     <div className="datatable-filter-demo">
       <div className="card">
-        <h5>Filter Menu</h5>
-        <p>Filters are displayed in an overlay.</p>
+        <Menubar model={menuItems} />
         <DataTable
-          value={customers1}
+          value={customers}
           paginator
           className="p-datatable-customers"
-          showGridlines
-          rows={10}
+          //showGridlines
+          rows={20}
           dataKey="id"
-          filters={filters1}
+          filters={filters}
           filterDisplay="menu"
-          loading={loading1}
+          loading={loading}
           responsiveLayout="scroll"
-          globalFilterFields={['name', 'country.name', 'representative.name', 'balance', 'status']}
-          header={header1}
-          emptyMessage="No customers found."
+          globalFilterFields={['first_name', 'last_name', 'phone', 'tc_no']}
+          header={header}
+          emptyMessage="Kayıt bulunamadı!"
           size="small"
+          selection={selectedProduct5}
+          onSelectionChange={(e) => setSelectedProduct5(e.value)}
         >
+          <Column selectionMode="single" headerStyle={{ width: '3em' }}></Column>
           <Column
-            field="name"
-            header="Name"
+            field="first_name"
+            header="Adı"
             filter
-            filterPlaceholder="Search by name"
+            filterPlaceholder="Ara"
             style={{ minWidth: '12rem' }}
           />
           <Column
-            header="Country"
-            filterField="country.name"
+            field="last_name"
+            header="Soyadı"
+            filter
+            filterPlaceholder="Ara"
             style={{ minWidth: '12rem' }}
-            body={countryBodyTemplate}
-            filter
-            filterPlaceholder="Search by country"
-            filterClear={filterClearTemplate}
-            filterApply={filterApplyTemplate}
-            filterFooter={filterFooterTemplate}
           />
           <Column
-            header="Agent"
-            filterField="representative"
-            showFilterMatchModes={false}
-            filterMenuStyle={{ width: '14rem' }}
-            style={{ minWidth: '14rem' }}
-            body={representativeBodyTemplate}
+            field="phone"
+            header="Telefon"
             filter
-            filterElement={representativeFilterTemplate}
+            filterPlaceholder="Ara"
+            style={{ minWidth: '12rem' }}
           />
           <Column
-            header="Date"
-            filterField="date"
+            field="tc_no"
+            header="TC"
+            filter
+            filterPlaceholder="Ara"
+            style={{ minWidth: '12rem' }}
+          />
+          <Column
+            header="Kayıt Tarihi"
+            filterField="created_at"
             dataType="date"
-            style={{ minWidth: '10rem' }}
+            style={{ minWidth: '12rem' }}
             body={dateBodyTemplate}
-            filter
-            filterElement={dateFilterTemplate}
+            // filter
+            // filterElement={dateFilterTemplate}
           />
-          <Column
+          {/* <Column
             header="Balance"
             filterField="balance"
             dataType="numeric"
@@ -427,35 +327,7 @@ const Customer = () => {
             body={balanceBodyTemplate}
             filter
             filterElement={balanceFilterTemplate}
-          />
-          <Column
-            field="status"
-            header="Status"
-            filterMenuStyle={{ width: '14rem' }}
-            style={{ minWidth: '12rem' }}
-            body={statusBodyTemplate}
-            filter
-            filterElement={statusFilterTemplate}
-          />
-          <Column
-            field="activity"
-            header="Activity"
-            showFilterMatchModes={false}
-            style={{ minWidth: '12rem' }}
-            body={activityBodyTemplate}
-            filter
-            filterElement={activityFilterTemplate}
-          />
-          <Column
-            field="verified"
-            header="Verified"
-            dataType="boolean"
-            bodyClassName="text-center"
-            style={{ minWidth: '8rem' }}
-            body={verifiedBodyTemplate}
-            filter
-            filterElement={verifiedFilterTemplate}
-          />
+          /> */}
         </DataTable>
       </div>
     </div>
